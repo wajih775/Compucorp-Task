299b2ec8cf2e7d140380a302f82df9b62b5b4997
wajih775 <wajihurrehman90@yahoo.com>
2016-10-16 07:39:46 +0500

    All Code

---
+++ app/app.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('mainApp', ['ui.bootstrap']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    
+         .when('/home',
+            {
+                controller: 'homeController',
+                templateUrl: 'app/templates/home.html'
+            })
+        .otherwise({ redirectTo: '/home' });
+});
